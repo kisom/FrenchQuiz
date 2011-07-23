6fc03b48f47930387dfafe04ecf5f7677393ebea
Kyle Isom <coder@kyleisom.net>
2011-07-23 11:52:44 -0400

    Initial Commit

---
+++ FrenchQuiz/FrenchQuizAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  FrenchQuizAppDelegate.h
+//  FrenchQuiz
+//
+//  Created by Kyle Isom on 7/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class FrenchQuizViewController;
+
+@interface FrenchQuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) FrenchQuizViewController *viewController;
+
+@end
---
+++ FrenchQuiz/FrenchQuizViewController.h
@@ -0,0 +1,13 @@
+//
+//  FrenchQuizViewController.h
+//  FrenchQuiz
+//
+//  Created by Kyle Isom on 7/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FrenchQuizViewController : UIViewController
+
+@end
